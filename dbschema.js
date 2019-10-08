ec286f2da02ee03c9d91fe3d9463f9603c5fd46a
datngusi <thainga0909@gmail.com>
2019-10-09 00:57:23 +0700

    sign-up

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  status: [
+    {
+      userHandle: "user",
+      body: "this is the sream body",
+      createdAt: "2019-03-15T11:46:01.018Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
-var serviceAccount = require("socialape-679d2-a51525b1dae0.json");  //this file is  in functions.node_modules get from firebase generate private key
+var serviceAccount = require("socialape-679d2-5d8e5605a56a.json"); //this file is  in functions.node_modules get from firebase generate private key
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://socialape-679d2.firebaseio.com/'
+  databaseURL: "https://socialape-679d2.firebaseio.com/"
 });
 
 const express = require("express");
 const app = express();
+const config = {
+  apiKey: "AIzaSyAyajOGRnnwAAFrxqm_EbkYD7sEJCRqOk0",
+  authDomain: "socialape-679d2.firebaseapp.com",
+  databaseURL: "https://socialape-679d2.firebaseio.com",
+  projectId: "socialape-679d2",
+  storageBucket: "socialape-679d2.appspot.com",
+  messagingSenderId: "571118632123",
+  appId: "1:571118632123:web:e58af60f2af9f972af3d9e",
+  measurementId: "G-9PRHEV7JY0"
+};
+const firebase = require('firebase');
+firebase.initializeApp(config);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -25,7 +37,12 @@ app.get("/status", (req, res) => {
     .then(data => {
       let status = [];
       data.forEach(doc => {
-        status.push(doc.data());
+        status.push({
+          statusId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(status);
     })
@@ -36,7 +53,7 @@ app.post("/status", (req, res) => {
   const newStatus = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
   admin
@@ -50,6 +67,23 @@ app.post("/status", (req, res) => {
       res.status(500).json({ error: "something went wrong" });
       console.error(err);
     });
+});
+
+app.post('/signup',(req, res) => {
+  const newUser ={
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then((data) => {
+    return res.status(201).json({message: `user ${data.user.uid} signed up successfully`});
+  })
+  .catch((err) => {
+    console.error(err);
+    return res.status(500).json({error: err.code});
+  });
 });
 
 // https://basaeurl.com/api/
